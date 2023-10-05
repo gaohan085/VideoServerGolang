02e17baaa3ece108240321c493d2fd7da01323b3
gaohan <gaohan085@gmail.com>
2023-10-05 23:27:16 +0800

    file reader handler get data from remote host while in development

--- handlers/file-reader-handler.go
+++ handlers/file-reader-handler.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/proxy"
 	_ "github.com/joho/godotenv/autoload"
 
 	"go-fiber-react-ts/lib"
@@ -28,50 +29,56 @@ type DirChildElem struct {
 }
 
 func FileReaderHandlers(c *fiber.Ctx) error {
-	rootDir := os.Getenv("ROOT_DIR")
-	nginxServAddr := os.Getenv("NGINX_SERVE_ADDRESS")
 	path, err := url.QueryUnescape(c.Params("*"))
-	if err != nil {
-		return c.Status(fiber.StatusNotFound).SendString(err.Error())
-	}
-	entries, err := os.ReadDir(rootDir + path)
-	if err != nil {
-		return c.Status(fiber.StatusNotFound).SendString(err.Error() + "ERROR OPEN" + " " + rootDir + c.Params("*"))
-	}
-
-	var elems = make([]DirChildElem, len(entries))
-
-	for index, entry := range entries {
-		var extName string
-		if strings.Contains(entry.Name(), ".") {
-			extName = entry.Name()[strings.LastIndex(entry.Name(), "."):]
+	switch os.Getenv("ENV") {
+	case "production":
+		rootDir := os.Getenv("ROOT_DIR")
+		nginxServAddr := os.Getenv("NGINX_SERVE_ADDRESS")
+		if err != nil {
+			return c.Status(fiber.StatusNotFound).SendString(err.Error())
 		}
-		var playSrc string
-		if lib.IsVideo(&extName) {
-			playSrc = nginxServAddr + path + "/" + entry.Name()
+		entries, err := os.ReadDir(rootDir + path)
+		if err != nil {
+			return c.Status(fiber.StatusNotFound).SendString(err.Error() + "ERROR OPEN" + " " + rootDir + c.Params("*"))
+		}
+
+		var elems = make([]DirChildElem, len(entries))
+
+		for index, entry := range entries {
+			var extName string
+			if strings.Contains(entry.Name(), ".") {
+				extName = entry.Name()[strings.LastIndex(entry.Name(), "."):]
+			}
+			var playSrc string
+			if lib.IsVideo(&extName) {
+				playSrc = nginxServAddr + path + "/" + entry.Name()
+			} else {
+				playSrc = ""
+			}
+			elems[index] = DirChildElem{Name: entry.Name(), IsFile: !entry.IsDir(), IsFolder: entry.IsDir(), ExtName: extName, PlaySrc: playSrc, CurrentPath: path + "/"}
+		}
+
+		var parentFolder string
+		var currentPath string
+		if path == "" {
+			parentFolder = ""
+			currentPath = "/"
+		} else if strings.Count(path, "/") == 0 {
+			parentFolder = "/"
+			currentPath = path
 		} else {
-			playSrc = ""
+			parentFolder = path[:strings.LastIndex(path, "/")]
+			currentPath = path
 		}
-		elems[index] = DirChildElem{Name: entry.Name(), IsFile: !entry.IsDir(), IsFolder: entry.IsDir(), ExtName: extName, PlaySrc: playSrc, CurrentPath: path + "/"}
+
+		time.Sleep(250 * time.Millisecond)
+		return c.Status(fiber.StatusOK).JSON(&Folder{
+			ParentFolder: parentFolder,
+			CurrentPath:  currentPath,
+			ChildElem:    elems,
+		})
+	default:
+		return proxy.Do(c, "http://192.168.1.11/api/"+path)
 	}
 
-	var parentFolder string
-	var currentPath string
-	if path == "" {
-		parentFolder = ""
-		currentPath = "/"
-	} else if strings.Count(path, "/") == 0 {
-		parentFolder = "/"
-		currentPath = path
-	} else {
-		parentFolder = path[:strings.LastIndex(path, "/")]
-		currentPath = path
-	}
-
-	time.Sleep(250 * time.Millisecond)
-	return c.Status(fiber.StatusOK).JSON(&Folder{
-		ParentFolder: parentFolder,
-		CurrentPath:  currentPath,
-		ChildElem:    elems,
-	})
 }
