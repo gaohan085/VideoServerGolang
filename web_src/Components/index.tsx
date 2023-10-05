e6d3f4d5adaba362d6e0b4eedf6d2e3f31aa6609
gaohan <gaohan085@gmail.com>
2023-10-05 23:24:47 +0800

    remove dep plyr-react

--- web_src/Components/index.tsx
+++ web_src/Components/index.tsx
@@ -1,7 +1,6 @@
 export * from "./file-element";
 export * from "./folder-element";
 export * from "./open-folder-container";
-export * from "./player";
 export * from "./spinner";
 export * from "./status-bar";
 export * from "./sidebar"
