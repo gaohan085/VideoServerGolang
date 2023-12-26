33fe3540b639ac50714452b4006bc5c4a48e1e04
gaohan <gaohan085@gmail.com>
2023-12-27 00:06:08 +0800

    fetcher encodeURL

--- web_src/lib/fetcher.ts
+++ web_src/lib/fetcher.ts
@@ -1,4 +1,4 @@
 import axios, { AxiosResponse } from "axios";
 
 export const fetcher = <T>(url: string) =>
-  axios.get<AxiosResponse<T>>(url).then((res) => res.data);
+  axios.get<AxiosResponse<T>>(encodeURI(url)).then((res) => res.data);
