af17c2728e8fa1fa2692f17585614bd6e2717a4a
gaohan <gaohan085@gmail.com>
2024-01-02 23:02:56 +0800

    remove unuse devdep

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 const isProduction = process.env.NODE_ENV === "production";
 
