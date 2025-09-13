c7f58258bdc307d3f8da423f49f55bb4c3e17286
gaohan <gaohan085@gmail.com>
2025-09-14 01:19:16 +0800

    style: fiber app new logger

---
+++ logger.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"io"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	fiberlog "github.com/gofiber/fiber/v2/log"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func LoggerRegister(app *fiber.App, usage string) {
+	//Global fiberlog
+	file, _ := os.OpenFile("./log/record.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	iw := io.MultiWriter(os.Stdout, file)
+	fiberlog.SetOutput(iw)
+
+	loggerConfigDev := logger.Config{
+		Format:     "[INFO] | PID:${pid} | [${time}] | ${ip} | ${status} | ${latency} | ${method} | ${path}\n",
+		TimeFormat: "2006/Jan/02 15:04:05",
+		TimeZone:   "Asia/Shanghai",
+	}
+	loggerConfigPro := logger.Config{
+		Format:     "[${time}] | ${ip} | ${status} | ${latency} | ${method} | ${path} | ${ua}\n",
+		TimeFormat: "2006/Jan/02 15:04:05",
+		TimeZone:   "Asia/Shanghai",
+		Output:     file,
+	}
+
+	switch usage {
+	case "ffmpeg":
+		app.Use(logger.New(loggerConfigDev))
+	case "dev":
+		app.Use(logger.New(loggerConfigDev))
+	default:
+		app.Use(logger.New(loggerConfigPro))
+	}
+}
