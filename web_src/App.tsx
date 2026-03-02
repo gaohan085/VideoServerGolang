16643a2c09f288569f88cccfb2bed4eb2b55163d
gaohan <gaohan085@gmail.com>
2026-03-02 13:36:43 +0800

    feat: create browser history

--- web_src/App.tsx
+++ web_src/App.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { createBrowserHistory, createRouter, RouterProvider } from "@tanstack/react-router";
 import { LazyMotion } from "motion/react";
 import React from "react";
 import { SWRConfig } from "swr";
@@ -9,7 +9,9 @@ import fetcher from "./lib/fetcher.ts";
 import { routeTree } from "./routeTree.gen.ts";
 
 const loadFeatures = () => import("./motionFeatures.ts").then(res => res.default);
-const router = createRouter({ routeTree });
+
+const browserHistory = createBrowserHistory()
+const router = createRouter({ routeTree, history: browserHistory });
 
 declare module "@tanstack/react-router" {
   interface Register {
