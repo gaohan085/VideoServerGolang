db01d942106ecdb84f38755df468ff6a5e1bed01
gaohan <gaohan085@gmail.com>
2026-03-02 13:37:20 +0800

    fix: remove spinner default font size

--- web_src/Components/spinner.tsx
+++ web_src/Components/spinner.tsx
@@ -3,10 +3,11 @@
 import { FcSynchronize } from "react-icons/fc";
 import styles from "./spinner.module.scss";
 
-const Spinner: React.FC<Readonly<{ fontSize?: number; cssStyles?: Record<string, string> }>> = (
+const Spinner: React.FC<{ fontSize?: number; cssStyles?: Record<string, string> }> = (
   {
-    fontSize, cssStyles,
-  } = { fontSize: 24 }) => {
+    fontSize,
+    cssStyles,
+  }) => {
   return (
     <span
       className={styles.spinner}
